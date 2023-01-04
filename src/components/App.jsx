8e07c3167b23f41bca39215fd5f9dd83d84cf742
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2023-01-04 09:50:11 -0500

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,62 @@
+import { nanoid } from 'nanoid';
 import React from 'react';
-import { ContactForm } from './ContactForm/ContactForm';
-//import { Filter } from './Filter/Filter';
+import ContactForm from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { Title } from './Title/Title';
 import { Box } from './App.styled';
-import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import useLocalStorage from './hooks/useLocalStorage';
 
 export default function App() {
-  const dispatch = useDispatch();
-  const contactsList = useSelector(state => state.contacts);
+  const items = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
 
-  // const addNewContact = (name, number) => {
-  //   const normalizedName = name.toLowerCase();
-  //   const checkName = contacts.some(contact =>
-  //     contact.name.toLowerCase().includes(normalizedName)
-  //   );
+  const [contacts, setContacts] = useLocalStorage('contacts', items);
+  const [filter, setFilter] = useState('');
 
-  //   const newContact = {
-  //     id: nanoid(),
-  //     name,
-  //     number,
-  //   };
+  const addNewContact = (name, number) => {
+    const normalizedName = name.toLowerCase();
+    const checkName = contacts.some(contact =>
+      contact.name.toLowerCase().includes(normalizedName)
+    );
 
-  //   return checkName
-  //     ? alert(`${name} is already in contacts`)
-  //     : setContacts(prevState => [...prevState, newContact]);
-  // };
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
 
-  // const deleteContact = contactId => {
-  //   setContacts(contacts.filter(contact => contact.id !== contactId));
-  // };
+    return checkName
+      ? alert(`${name} is already in contacts`)
+      : setContacts(prevState => [...prevState, newContact]);
+  };
 
-  // const filterNames = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+  };
+
+  const filterNames = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
 
   return (
     <Box>
       <Title title="Phonebook" />
-      <ContactForm />
+      <ContactForm onSubmit={addNewContact} />
       <Title title="Contacts" />
-      {/* <Filter /> */}
-      <ContactList />
+      <Filter
+        value={filter}
+        onChange={event => setFilter(event.target.value)}
+      />
+      <ContactList contacts={filterNames()} onDeleteContact={deleteContact} />
     </Box>
   );
 }
